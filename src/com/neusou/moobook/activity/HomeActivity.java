4b2076fabaee4f4fb1ef0ecf085875e6bac96914
Agus Santoso <asantoso@wicube.(none)>
2010-05-27 07:43:02 +0700

    experimental

--- src/com/neusou/moobook/activity/HomeActivity.java
+++ src/com/neusou/moobook/activity/HomeActivity.java
@@ -328,7 +328,9 @@ public class HomeActivity extends BaseActivity {
 			}
 		};
 				
-		mFacebook = Facebook.getInstance();
+		mFacebook = Facebook.getInstance(); 
+		
+		///
 		
 		mTopHeaderViewFactory = new ViewFactory() {
 			@Override
